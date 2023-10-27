a7a444310ffe13de29d2f2fcb807cdf1547d394f
stella <sftamesis13@gmail.com>
2023-10-27 10:34:36 -0700

    setup import for routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/routes';
 
 const app = express();
 const PORT = 3000;//port our server is going to run
+
+routes(app);
 
 app.get('/', (req, res) => 
     res.send(`Node and express server is running on port ${PORT}`)
